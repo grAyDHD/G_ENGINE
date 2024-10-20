b6d38fb383fc59f8445d7f98bc22944c45fe9d0d
Gray <graydhdsquared@gmail.com>
2024-10-20 13:56:09 -0500

    setup sequence of projects/implementations covering basic concepts

--- 15_m3Animate/C/15_m3Animate.c
+++ 15_m3Animate/C/15_m3Animate.c
@@ -4,31 +4,37 @@
 extern void SpriteFrame(int x, int y, int frame, const void *image,
                         int frameCount);
 
+void animation(int x, int y, int frame, const void *image) {
+  for (int w = 0; w < 15; w++) {
+    for (int h = 0; h < 10; h++) {
+      if (w > 0)
+        SpriteFrame(x + (w * 16), y + (h * 16), frame, image, 4);
+      if (w == 0)
+        SpriteFrame(0, y + (h * 16), frame, image, 4);
+    }
+  }
+}
+
 int main() {
   DSPC = MODE3 | BG2;
 
-  int pX = 0, pY = 0;
   while (1) {
     VBLANK();
-    for (volatile int x = 0; x < 2000; x++)
+    for (volatile int z = 0; z < 30000; z++)
       ;
-    SpriteFrame(pX, pY, 0, lil_guyBitmap, 4);
-    pX += 16;
+    animation(0, 0, 0, lil_guyBitmap);
 
-    for (volatile int x = 0; x < 2000; x++)
+    for (volatile int z = 0; z < 30000; z++)
       ;
-    SpriteFrame(pX, pY, 1, lil_guyBitmap, 4);
-    pX += 16;
+    animation(0, 0, 1, lil_guyBitmap);
+    for (volatile int z = 0; z < 30000; z++)
+      ;
+    animation(0, 0, 2, lil_guyBitmap);
 
-    for (volatile int x = 0; x < 2000; x++)
+    for (volatile int z = 0; z < 30000; z++)
       ;
-    SpriteFrame(pX, pY, 2, lil_guyBitmap, 4);
-    pX += 16;
-
-    for (volatile int x = 0; x < 2000; x++)
-      ;
-    SpriteFrame(pX, pY, 3, lil_guyBitmap, 4);
-    pX += 16;
+    animation(0, 0, 3, lil_guyBitmap);
   }
+
   return 0;
 }
