149d177d26b4a214bfdff00e597d2ad7b1c116b2
grAyDHD <graydhdsquared@gmail.com>
2024-11-07 21:16:06 -0500

    setup steps for 17_m3Paint underway

--- Library/demos/17_m3Paint/C/17_m3Paint.c
+++ Library/demos/17_m3Paint/C/17_m3Paint.c
@@ -1,8 +1,13 @@
 #include "draw.h"
 #include "gfx.h"
 
+enum MODE { DRAWING = 0, COLOR_SELECT, SHAPE_SELECT };
+
 int main() {
   DSPC = MODE3 | BG2;
   fillScreen(dblClr(RGB(3, 5, 9)));
+
+  //  drawRect({8, 8}, 4, 32, color);
+
   return 0;
 }
