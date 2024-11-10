39518b65e00fe615fb917902f02df4ea0a88d2d4
grAyDHD <graydhdsquared@gmail.com>
2024-11-09 20:31:50 -0500

    updated about file to design doc and created first compilable ROM, drawing a single small black square in top left corner over a blue background

--- Library/demos/17_m3Paint/C/17_m3Paint.c
+++ Library/demos/17_m3Paint/C/17_m3Paint.c
@@ -1,7 +1,8 @@
 #include "draw.h"
 #include "gfx.h"
+#include "typedefs.h"
 
-enum MODE { DRAWING = 0, PAUSE, UNPAUSE, COLOR, SHAPE } MODE;
+enum MODE { DRAWING = 0, COLOR, SHAPE } MODE;
 enum COLOR_SELECT { RED = 0, GREEN, BLUE };
 enum SHAPE_SELECT { SQUARE, TRIANGLE, CIRCLE, HEXAGON };
 
@@ -14,79 +15,33 @@ u16 color = 0x0000;
 // blue  0x0000 0011 1110 0000
 // green 0x0111 1100 0000 0000
 
+void fillSquare(Coordinate cursor, int clr) {
+  drawRect(cursor, 4, 4, clr);
+  drawRect(cursor, 3, 3, clr);
+  drawRect(cursor, 2, 2, clr);
+  drawRect(cursor, 1, 1, clr);
+}
+
 int main() {
   DSPC = MODE3 | BG2;
   // up/down cycle blue value
   fillScreen(dblClr(RGB(3, 5, 9)));
 
   enum MODE appState = DRAWING;
-  enum COLOR_SELECT colorMode = RED;
-  enum SHAPE_SELECT brushShape = SQUARE;
+  //  enum COLOR_SELECT colorMode = RED;
+  //  enum SHAPE_SELECT brushShape = SQUARE;
+
+  Coordinate cursor = {0, 0};
 
   while (1) {
     switch (appState) {
     case (DRAWING):
-      // input START sets appState to PAUSE
-      break;
-    case (PAUSE):
-      // wait briefly, set up for COLOR state
-      // LT for COLOR mode, RT for SHAPE mode
+      // input START sets appState to COLOR
+      fillSquare(cursor, color);
       break;
     case (COLOR):
-      switch (colorMode) {
-        // each colorMode individual color value change reflected in test square
-        // white regions of test square to be drawn as RGB15
-      case (RED):
-        while (colorMode == RED && appState == COLOR) {
-          u8 clr5 = 0;
-          if (keyDown(UP)) {
-            // if clr5 < 31, clr5++, wait- use key repeater to repeat every half
-            // second?
-          }
-          if (keyDown(DOWN)) {
-
-            // if clr5 > 0, clr5 --
-          }
-          if (keyDown(RIGHT)) {
-            // colorMode = green
-          }
-          if (keyDown(RT)) {
-            appState = SHAPE;
-          }
-        }
-        // up/down cycle red value ranging 0 to 31
-        // whitish cursor, moves 2 pixels at a time upwards, 64 px tall
-        // right, mode = GREEN
-        break;
-
-      case (GREEN):
-        // up/down cycle green value
-        // left: red mode, right: blue mode
-        break;
-
-      case (BLUE):
-        // up/down cycle blue value
-        // left: green mode
-        break;
-      }
       break;
-
     case (SHAPE):
-      switch (brushShape) {
-        // only options here are left/right to toggle shapes for brushShape
-      case (SQUARE):
-        break;
-      case (TRIANGLE):
-        break;
-      case (CIRCLE):
-        break;
-      case (HEXAGON):
-        break;
-      }
-
-      break;
-    case (UNPAUSE):
-      // here
       break;
     }
   }
@@ -95,3 +50,56 @@ int main() {
 
   return 0;
 }
+/*
+case (COLOR):
+switch (colorMode) {
+// each colorMode individual color value change reflected in test square
+// white regions of test square to be drawn as RGB15
+case (RED):
+
+while (colorMode == RED && appState == COLOR) {
+u8 clr5 = 0;
+if (keyDown(UP)) {
+// if clr5 < 31, clr5++, wait- use key repeater to repeat every half
+// second?
+}
+if (keyDown(DOWN)) {
+
+// if clr5 > 0, clr5 --
+}
+if (keyDown(RIGHT)) {
+// colorMode = green
+}
+if (keyDown(RT)) {
+appState = SHAPE;
+}
+
+// up/down cycle red value ranging 0 to 31
+// whitish cursor, moves 2 pixels at a time upwards, 64 px tall
+// right, mode = GREEN
+//
+case (GREEN):
+// up/down cycle green value
+// left: red mode, right: blue mode
+break;
+
+case (BLUE):
+// up/down cycle blue value
+// left: green mode
+break;
+}
+break;
+
+case (SHAPE):
+switch (brushShape) {
+// only options here are left/right to toggle shapes for brushShape
+case (SQUARE):
+break;
+case (TRIANGLE):
+break;
+case (CIRCLE):
+break;
+case (HEXAGON):
+break;
+}
+  */
