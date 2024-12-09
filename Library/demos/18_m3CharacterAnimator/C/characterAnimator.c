dd7cefc5697d237a6a11b9dbd82e4a986b12f79a
grAyDHD <graydhdsquared@gmail.com>
2024-12-09 02:39:17 -0500

    put structural notes into C file for quick reference when feature implementation begins.

--- Library/demos/18_m3CharacterAnimator/C/characterAnimator.c
+++ Library/demos/18_m3CharacterAnimator/C/characterAnimator.c
@@ -1,8 +1,52 @@
 #include "../includes/characterAnimator.h"
 #include "gfx.h"
+#include "typedefs.h"
+
+// NPC
+//  DIRECTION, Up or down when walking, with a check for position when to turn
+//  around
+//*  NPC only moves up and down, but direction can become left or right if
+// player interacts from that direction */
+
+// PLAYER
+//*  DIRECTION updates to last dpad input.
+//
+//
+
+// create struct for character,to extend to player and npc
+
+typedef enum { UP, DOWN, LEFT, RIGHT } DIRECTION;
+typedef enum { IDLE, WALK, TALKING } STATE;
+
+typedef struct {
+  DIRECTION direction;
+  STATE state;
+  Coordinate coordinate;
+} Character;
 
 int main() {
   DSPC = MODE3 | BG2;
-
+  // Start by drawing a background, with cache for each player.
+  // decide upon sprites to use until making own
+  // cfind asm func to draw animation frame, adapt to meet needs of this program
+  // NPC BEHAVIOR:
+  // draw at npc.coordinates
+  // if direction + UP and sstate = up, y- 1
+  // check step counter, of = 2, state now is DOWN.  then y +1.
+  // reset counter to zero immediately on changing direction
+  // PLAYER BEHAVIORE:
+  // if dpad is down, add 8 directionmovement?
+  // if not, last dpad input  is DIRECTION
+  // if any dpad is down, STATE is walking
+  // if facing exact square of NPC and pressing A, TALKING
+  // when no DPAD is pressed STATE is idle.
+  //
+  // calculate grid based on character size?
+  // have asm function take grid into accountto take player x and yproperly, is
+  // this necessary?  might mike fefinements easier. find ggood but simple 3
+  // four frame walking animation
+  while (1) {
+    VBLANK();
+  }
   return 0;
 }
