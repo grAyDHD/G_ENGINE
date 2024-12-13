485d845bf85bff022b06274a260e063247cf2773
grAyDHD <graydhdsquared@gmail.com>
2024-12-12 22:52:32 -0500

    animated chocobo walking to the right

--- Library/demos/18_m3CharacterAnimator/C/characterAnimator.c
+++ Library/demos/18_m3CharacterAnimator/C/characterAnimator.c
@@ -1,4 +1,5 @@
 #include "../includes/characterAnimator.h"
+#include "timer.h"
 
 #define chocoboX chocobo.coordinate.x
 #define chocoboY chocobo.coordinate.y
@@ -13,10 +14,40 @@ int main() {
       .state = WALK,
       .coordinate = {80, 75},
   };
+
   initializeCharacterSprites(&chocobo, chocoboSprites);
 
   while (1) {
     VBLANK();
+    // draw chocobo frame 0
+    SpriteFrame32Bit(chocoboX, chocoboY, 0, chocobo.currentSpriteSheet, 4);
+    restoreFrameBackground(chocoboX, chocoboY, 32, BedroomBitmap);
+    simpleWait(100);
+
+    VBLANK();
+    // step right draw frame 1
+    clearSpriteFrame(chocoboX, chocoboY, 32, BedroomBitmap);
+    chocobo.coordinate.x += 4;
+    SpriteFrame32Bit(chocoboX, chocoboY, 1, chocobo.currentSpriteSheet, 4);
+    restoreFrameBackground(chocoboX, chocoboY, 32, BedroomBitmap);
+    simpleWait(100);
+
+    VBLANK();
+    // step right draw frame 2
+
+    clearSpriteFrame(chocoboX, chocoboY, 32, BedroomBitmap);
+    chocobo.coordinate.x += 4;
+    SpriteFrame32Bit(chocoboX, chocoboY, 2, chocobo.currentSpriteSheet, 4);
+    restoreFrameBackground(chocoboX, chocoboY, 32, BedroomBitmap);
+    simpleWait(100);
+    VBLANK();
+    // step draw frame 3
+
+    clearSpriteFrame(chocoboX, chocoboY, 32, BedroomBitmap);
+    chocobo.coordinate.x += 4;
+    SpriteFrame32Bit(chocoboX, chocoboY, 3, chocobo.currentSpriteSheet, 4);
+    restoreFrameBackground(chocoboX, chocoboY, 32, BedroomBitmap);
+    simpleWait(100);
   }
 
   return 0;
