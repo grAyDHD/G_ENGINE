63fbaab15459af4d20960e67e540c46e979ad3b2
grAyDHD <graydhdsquared@gmail.com>
2024-12-16 17:25:19 -0500

    fixed assembly function, can draw one of three characters on 16x32 pixel bitmap, A B and C

--- Library/demos/19_m3Text/asm/copyGlyphToVRAM.s
+++ Library/demos/19_m3Text/asm/copyGlyphToVRAM.s
@@ -17,34 +17,43 @@ copyGlyphToVRAM:
   ldr r1, =0x6000000  @ Base VRAM address
   add r1, r1, r0      @ VRAM address for the top-left corner of the sprite
 
-                  @ r1 = VRAM xy address
-                  @ r2 = image pointer
+
   @ Initialize constants for character width and row offsets
-  mov r4, #10         @ Character width
-  mov r5, #460        @ VRAM row offset
-  mov r6, #22         @ image row offset
+  mov r4, #8          @ Character width
+  mov r5, #480        @ VRAM row offset
+  sub r5, r5, r4
+  sub r5, r5, r4
+  mov r6, #64         @ image row offset
+  sub r6, r6, r4
+  sub r6, r6, r4
 
   @ Initialize row counter
-  mov r7, #1
+  mov r7, #16
   
+                  @ r0 = vram byte offset amount (not needed)
+                  @ r1 = VRAM xy address
+                  @ r2 = image pointer
+                  @ r3 = empty
+                  @ r4 = character width
+                  @ r5 = VRAM row offset
+                  @ r6 = image row offset
+  add r2, r2, #0
+  @A offset = 0, B offset = 12, C offset =
 
 .LoopRow:
-  ldr r2, [r2], #4    @ Load 2 pixels, increment image pointer
-  str r1, [r1], #4    @ Store 2 pixels, increment VRAM pointer
-  ldr r2, [r2], #4    @ Load 2 pixels, increment image pointer
-  str r1, [r1], #4    @ Store 2 pixels, increment VRAM pointer
-  ldr r2, [r2], #4    @ Load 2 pixels, increment image pointer
-  str r1, [r1], #4    @ Store 2 pixels, increment VRAM pointer
-  ldr r2, [r2], #4    @ Load 2 pixels, increment image pointer
-  str r1, [r1], #4    @ Store 2 pixels, increment VRAM pointer
-  ldr r2, [r2], #4    @ Load 2 pixels, increment image pointer
-  str r1, [r1], #4    @ Store 2 pixels, increment VRAM pointer
+  ldr r3, [r2], #4    @ Load 2 pixels, increment image address
+  str r3, [r1], #4    @ Store 2 pixels, increment VRAM address
+  ldr r3, [r2], #4    @ Load 2 pixels, increment image address
+  str r3, [r1], #4    @ Store 2 pixels, increment VRAM address
+  ldr r3, [r2], #4    @ Load 2 pixels, increment image address
+  str r3, [r1], #4    @ Store 2 pixels, increment VRAM address
+  ldr r3, [r2], #4    @ Load 2 pixels, increment image address
+  str r3, [r1], #4    @ Store 2 pixels, increment VRAM address
 
-@  add r2, r2, r5      @ Add VRAM row offset to VRAM pointer
-@  add r1, r1, r6      @ Add image row offset to image pointer
-   
+  add r1, r1, r5      @ Add VRAM row offset to VRAM address
+  add r2, r2, r6      @ Add image row offset to image address  
 
-  subs r3, r3, #1     @ Decrement row counter
+  subs r7, r7, #1     @ Decrement row counter
   bne .LoopRow        @ Repeat until all 16 rows are processed
 
   pop {r4-r11}        @ Restore registers
