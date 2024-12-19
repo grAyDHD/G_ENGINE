53220efc98bdcb42d4e916376c39f84c96757cbb
grAyDHD <graydhdsquared@gmail.com>
2024-12-19 13:54:24 -0500

    printDebug well into minimal implementation, must debug printed value not being proper hex format

--- Library/demos/19_m3Text/C/m3Text.c
+++ Library/demos/19_m3Text/C/m3Text.c
@@ -5,17 +5,20 @@
 int getFontDataIndex(char c);
 
 void printString(int x, int y, const char *text);
-void printDebug(int x, int y, char *text, u16 val1, u16 val2);
+void printDebug(int x, int y, const char *format, int arg1, int arg2);
+void itoh(int number, char *buffer);
 
 int main() {
   DSPC = MODE3 | BG2;
   int bgColor = dblClr(RGB(17, 13, 28));
   fillScreen(bgColor);
 
-  const char *text = "Hello World!";
+  // const char *text = "Hello World!";
   int x = 20;
   int y = 20;
-  printString(x, y, text);
+  //  printString(x, y, text);
+
+  printDebug(x, y, "pba = 0x%08x", (unsigned int)&PeaberryBitmap, 0);
   while (1) {
   }
   return 0;
@@ -37,12 +40,6 @@ void printString(int x, int y, const char *text) {
       x += fontData[glyphIndex].width + 1;
     }
   }
-};
-
-void printDebug(int x, int y, char *text, u16 val1, u16 val2) {
-  char stringOutput[64];
-
-  printString(x, y, stringOutput);
 };
 
 int getFontDataIndex(char c) {
@@ -122,4 +119,47 @@ int getFontDataIndex(char c) {
       return -1;
     }
   }
+}
+
+void itoh(int number, char *buffer) {
+  const char hexChars[] = "0123456789ABCDEF";    // Lookup array for hex digits
+  for (int i = 0; i < 8; i++) {                  // Process 8 nibbles (32 bits)
+    int nibble = (number >> (28 - i * 4)) & 0xF; // Extract nibble
+    buffer[i] = hexChars[nibble];                // Convert to hex char
+  }
+  buffer[8] = '\0'; // Null-terminate the string
+}
+
+void printDebug(int x, int y, const char *format, int arg1, int arg2) {
+  char stringOutput[64];
+  int outputIndex = 0;
+  int arg = 1;
+  char formatBuffer[9];
+
+  while (*format) {
+    if (*format == '%') {
+      format++;
+      if (*format == 'x') {
+        if (arg == 1) {
+          itoh(arg1, formatBuffer);
+          arg = 2;
+        } else {
+          itoh(arg2, formatBuffer);
+        }
+        for (int i = 0; formatBuffer[i] != '\0'; i++) {
+          stringOutput[outputIndex++] = formatBuffer[i];
+        }
+        format++; // move past 'x'
+      } else {
+        // for now, add '%' to output and continue
+        stringOutput[outputIndex++] = '%';
+      }
+    } else {
+
+      stringOutput[outputIndex++] = *format++;
+    }
+  }
+
+  stringOutput[outputIndex] = '\0'; // Null terminat output
+  printString(x, y, stringOutput);  // Display the final string
 }
