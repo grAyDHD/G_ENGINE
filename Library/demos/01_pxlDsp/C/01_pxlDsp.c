b55b84089c0d247376fa24acc86d090d58519ec3
grAyDHD <graydhdsquared@gmail.com>
2024-12-24 00:23:35 -0500

    reverted project 01 c file, prep 03 circle draw for assembly optimization

--- Library/demos/01_pxlDsp/C/01_pxlDsp.c
+++ Library/demos/01_pxlDsp/C/01_pxlDsp.c
@@ -1,17 +1,5 @@
-#include "core/typedefs.h"
-
-#define DSPC *(u32 *)0x4000000
-#define VRAM (u16 *)0x6000000
-#define SW 240
-#define SH 160
-#define MODE3 0x0003
-#define BG2 0x0400
-#define RGB(r, g, b) (((r) & 0x1F) | ((g) & 0x1F) << 5 | ((b) & 0x1F) << 10)
-
-void plotPixel(int x, int y, u16 clr) {
-  ((u16 *)VRAM)[y * SW + x] = clr;
-} // mode 3
-//
+#include "graphics/draw.h"
+#include "graphics/video.h"
 
 int main() {
   DSPC = (MODE3 | BG2);
