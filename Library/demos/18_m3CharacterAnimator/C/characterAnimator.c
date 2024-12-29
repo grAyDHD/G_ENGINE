6514ae47a88e18c9e21d5cf00c452582bbdb44f7
grAyDHD <graydhdsquared@gmail.com>
2024-12-29 02:51:37 -0500

    ECS in project 18 working with player movement

--- Library/demos/18_m3CharacterAnimator/C/characterAnimator.c
+++ Library/demos/18_m3CharacterAnimator/C/characterAnimator.c
@@ -1,10 +1,57 @@
 #include "../includes/characterAnimator.h"
 #include "Bedroom.h"
 #include "graphics/video.h"
+#include "input/in.h"
 
 static ComponentManager world;
 static Entity entities[MAX_ENTITIES];
 static int nextEntityId = 0;
+
+int createEntity(int componentMask);
+int createPlayer(const void *spriteSheet);
+void renderPlayer(int playerId, ComponentManager *world);
+
+int main() {
+  DSPC = MODE3 | BG2;
+  int playerId = createPlayer(&RoboBitmap);
+
+  while (1) {
+    updateKeys();
+    VBLANK();
+
+    clearSpriteFrame(world.position[playerId].x, world.position[playerId].y, 32,
+                     BedroomBitmap);
+
+    // set state/direction
+    //  check for collision, if none move position
+    if (keyDown(U)) {
+      world.position[playerId].y -= 1;
+      world.animation[playerId].state = WALK;
+      world.animation[playerId].direction = UP;
+    } else if (keyDown(D)) {
+      world.position[playerId].y += 1;
+      world.animation[playerId].state = WALK;
+      world.animation[playerId].direction = DOWN;
+    } else if (keyDown(L)) {
+      world.position[playerId].x -= 1;
+      world.animation[playerId].state = WALK;
+      world.animation[playerId].direction = LEFT;
+    } else if (keyDown(R)) {
+      world.position[playerId].x += 1;
+      world.animation[playerId].state = WALK;
+      world.animation[playerId].direction = RIGHT;
+    }
+
+    renderPlayer(playerId, &world);
+
+    if (keyReleased(U) | keyReleased(D) | keyReleased(L) | keyReleased(R)) {
+      world.animation[playerId].frameNumber = 0;
+      world.animation[playerId].state = IDLE;
+    }
+  }
+
+  return 0;
+}
 
 int createEntity(int componentMask) {
   if (nextEntityId >= MAX_ENTITIES) {
@@ -40,15 +87,4 @@ int createPlayer(const void *spriteSheet) {
 void renderPlayer(int playerId, ComponentManager *world) {
   SpriteFrame32Bit(&world->position[playerId], &world->animation[playerId],
                    world->sprite[playerId].spriteSheet);
-}
-
-int main() {
-  DSPC = MODE3 | BG2;
-  int playerId = createPlayer(&RoboBitmap);
-
-  while (1) {
-    VBLANK();
-    renderPlayer(playerId, &world);
-  }
-  return 0;
 }
