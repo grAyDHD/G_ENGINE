2124b1638a64adec4e6c86156654bd5d73376cb8
grAyDHD <graydhdsquared@gmail.com>
2025-01-01 02:52:59 -0500

    updated with outline for changes commented in main

--- Library/demos/18_m3CharacterAnimator/C/characterAnimator.c
+++ Library/demos/18_m3CharacterAnimator/C/characterAnimator.c
@@ -19,12 +19,17 @@ int main() {
   while (1) {
     VBLANK();
 
+    // refactor into refreshRenderSystem(&entitySystem);
     clearSpriteFrame(world.position[playerId].x, world.position[playerId].y, 32,
                      BedroomBitmap);
     clearSpriteFrame(world.position[playerId + 1].x, world.position[playerId + 1].y, 32, BedroomBitmap);
-    updateInputSystem(&entitySystem, &world);
-    updateBehaviorSystem(&entitySystem, &world);
 
+    // update following 2 to update velocity rather than position, v system will handle movement
+    updateInputSystem(&entitySystem, &world); // updates player position along with other input entities various params as long as INPUT_ENABLED 
+    updateBehaviorSystem(&entitySystem, &world); // updates entities with AI
+    //  should collisions be checked in player input function, and ai behavior functions?
+    // annoyting part, hitbox size. I'll add this later cuz fuck that lolz
+    // updateMovementSystem(&entitySystem);
     
     updateRenderSystem(&entitySystem, &world);
 
