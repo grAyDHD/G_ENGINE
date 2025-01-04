1607431effa9695de397c3e72be43bacf8315263
grAyDHD <graydhdsquared@gmail.com>
2025-01-04 05:00:59 -0500

    inlined collision check function

--- Library/src/C/ecs/systems.c
+++ Library/src/C/ecs/systems.c
@@ -27,47 +27,37 @@ void updateRenderSystem(ECS *ecs, ComponentStorage *components) {
   }
 };
 
-int checkForCollision(PositionComponent *position[2],
-                      VelocityComponent *velocity[2],
-                      HitboxComponent *hitbox[2]) {
-  int Ax1 = position[0]->x + velocity[0]->dx;
-  int Ax2 = Ax1 + hitbox[0]->width;
-  int Ay1 = position[0]->y + velocity[0]->dy;
-  int Ay2 = Ay1 + hitbox[0]->height;
-
-  int Bx1 = position[1]->x;
-  int Bx2 = Bx1 + hitbox[1]->width;
-  int By1 = position[1]->y;
-  int By2 = By1 + hitbox[1]->height;
-
+static inline int
+checkForCollision(PositionComponent *posA, VelocityComponent *velA,
+                  HitboxComponent *hitA, PositionComponent *posB,
+                  VelocityComponent *velB, HitboxComponent *hitB) {
+  int Ax1 = posA->x + velA->dx;
+  int Ax2 = Ax1 + hitA->width;
+  int Ay1 = posA->y + velA->dy;
+  int Ay2 = Ay1 + hitA->height;
+  int Bx1 = posB->x;
+  int Bx2 = Bx1 + hitB->width;
+  int By1 = posB->y;
+  int By2 = By1 + hitB->height;
   return (Bx2 > Ax1 && Bx1 < Ax2 && By2 > Ay1 && By1 < Ay2);
 }
 
 void updateCollisionSystem(ECS *ecs, ComponentStorage *components) {
   for (int id = 0; id < MAX_ENTITIES; id++) {
-    ecs->entity[id].flag &= ~COLLISION_DETECTED; // Clear the collision flag
+    ecs->entity[id].flag &= ~COLLISION_DETECTED;
   }
 
   for (int idA = 0; idA < MAX_ENTITIES; idA++) {
     if (ecs->entity[idA].flag & ENABLE_COLLISIONS) {
       for (int idB = idA + 1; idB < MAX_ENTITIES; idB++) {
         if (ecs->entity[idB].flag & ENABLE_COLLISIONS) {
-
-          VelocityComponent *velA = &components->velocity[idA];
-          VelocityComponent *velB = &components->velocity[idB];
-          PositionComponent *posA = &components->position[idA];
-          PositionComponent *posB = &components->position[idB];
-          HitboxComponent *hitA = &components->hitbox[idA];
-          HitboxComponent *hitB = &components->hitbox[idB];
-
-          VelocityComponent *velocity[2] = {velA, velB};
-          PositionComponent *position[2] = {posA, posB};
-          HitboxComponent *hitbox[2] = {hitA, hitB};
-
-          if (checkForCollision(position, velocity, hitbox)) {
+          if (checkForCollision(
+                  &components->position[idA], &components->velocity[idA],
+                  &components->hitbox[idA], &components->position[idB],
+                  &components->velocity[idB], &components->hitbox[idB])) {
             ecs->entity[idA].flag |= COLLISION_DETECTED;
             ecs->entity[idB].flag |= COLLISION_DETECTED;
-          };
+          }
         }
       }
     }
