cf440ad97b681f92f35980c73a6e9cedcb81dacd
grAyDHD <graydhdsquared@gmail.com>
2025-05-08 19:05:51 -0400

    created project for printf demo

---
+++ Library/demos/21_m3printf/C/21_m3printf.c
@@ -0,0 +1,8 @@
+#include "../includes/21_m3printf.h"
+#include "gfx.h"
+
+int main() {
+    DSPC = MODE3 | BG2;
+
+    return 0;
+}
---
+++ Library/demos/21_m3printf/includes/21_m3printf.h
@@ -0,0 +1,12 @@
+#ifndef 21_M3PRINTF_H
+#define 21_M3PRINTF_H
+
+// Project-specific constants and declarations
+#define PROJECT_NAME "21_m3printf"
+
+// Video mode configuration
+#define PROJECT_VIDEO_MODE MODE3 | BG2
+
+// Add more project-specific macros or declarations here
+
+#endif // 21_M3PRINTF_H
