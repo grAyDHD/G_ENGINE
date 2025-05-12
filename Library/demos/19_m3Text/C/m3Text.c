20f9d39cd33bcf683fa93712001eb40a56063cad
grAyDHD <graydhdsquared@gmail.com>
2025-05-12 15:28:23 -0400

    outlined integer parsing/conversion for gprintf, fixed error in font table data

--- Library/demos/19_m3Text/C/m3Text.c
+++ Library/demos/19_m3Text/C/m3Text.c
@@ -5,6 +5,7 @@
 
 int getFontDataIndex(char c);
 void printString(int x, int y, const char *text);
+void gprintf(int x, int y, const char* format, int arg);
 
 int main() {
   DSPC = MODE3 | BG2;
@@ -14,7 +15,8 @@ int main() {
   const char *text = "Hello World!";
   int x = 20;
   int y = 20;
-  printString(x, y, text);
+  printString(x, y, "0123");
+//  gprintf(x, y, "hello %d", 0);
   while (1) {
   }
   return 0;
@@ -22,7 +24,6 @@ int main() {
 
 void printString(int x, int y, const char *text) {
   int glyphIndex;
-
   for (int i = 0; text[i] != '\0'; i++) {
     char c = text[i];
 
@@ -37,6 +38,99 @@ void printString(int x, int y, const char *text) {
     }
   }
 };
+
+//printing an int value, if < 0 and > -10, print '-' and digit
+//if > 0 < 10, print digit
+//if > 9 < 100, print 2 digits, etc.
+
+void gprintf(int x, int y, const char *fmt, int arg) {
+  int glyphIndex;
+  char c;
+  char buf[8] = {0};
+  int counter;
+  int powers[] = {10000, 1000, 100, 10, 1};
+  int leadingZero = 0; 
+
+  for (int i = 0; fmt[i] != '\0'; i++) {
+    if (fmt[i] == '%') {
+      i++;
+
+      switch (fmt[i]) {
+      case ('d'): 
+
+        if (arg == 0) {
+            // Special case for zero
+          buf[0] = '1';
+          glyphIndex = getFontDataIndex('0');
+          if (glyphIndex >= 0) {
+            copyGlyphToVRAM(x, y, &PeaberryBitmap, &fontData[glyphIndex]);
+            x += fontData[glyphIndex].width + 1;
+          }
+
+        } else if (arg < 0) {
+          c = '-';
+          glyphIndex = getFontDataIndex(c);
+          if (glyphIndex >= 0) {
+            copyGlyphToVRAM(x, y, &PeaberryBitmap, &fontData[glyphIndex]);
+            x += fontData[glyphIndex].width + 1;
+          }
+          arg = -arg;
+
+          } else {
+            for (int j = 0; j < 5; j++) {
+              counter = 0;
+              while (arg > powers[j]) {
+                arg -= powers[j];
+                counter++;
+              }
+              if (!(counter | buf[0])) {
+                leadingZero++;
+              } else {
+                buf[j - leadingZero] = '0' + counter;
+              }
+            }
+
+            for (int j = 0; j < (5 - leadingZero); j++) {
+                glyphIndex = getFontDataIndex(buf[j]);
+                if (glyphIndex >= 0) {
+                  copyGlyphToVRAM(x, y, &PeaberryBitmap, &fontData[glyphIndex]);
+                  x += fontData[glyphIndex].width + 1;
+              }
+            }
+          }
+
+          i++;
+          break;
+
+        case ('x'):
+        
+           // i++
+          break;
+        default:
+          c = '%';
+          glyphIndex = getFontDataIndex(c);
+          if (glyphIndex >= 0) {
+            copyGlyphToVRAM(x, y, &PeaberryBitmap, &fontData[glyphIndex]);
+            x += fontData[glyphIndex].width + 1;
+          }
+          break;
+      }
+    }
+
+    char c = fmt[i];
+    if (c == ' ') {
+      x += 5;
+      continue;
+    }
+
+    glyphIndex = getFontDataIndex(c);
+    if (glyphIndex >= 0) {
+      copyGlyphToVRAM(x, y, &PeaberryBitmap, &fontData[glyphIndex]);
+      x += fontData[glyphIndex].width + 1;
+    }
+  }
+}
+
 
 int getFontDataIndex(char c) {
   if (c >= 'A' && c <= 'Z') {
@@ -115,5 +209,5 @@ int getFontDataIndex(char c) {
       return -1;
     }
   }
-  void printString(int x, int y, const char *text);
 }
+
