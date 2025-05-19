573d2c58f52b79114dd817bf4b5a15c47cca5420
grAyDHD <graydhdsquared@gmail.com>
2025-05-19 19:30:35 -0400

    initialized new project, will update project creation script

--- Library/demos/21_debug/C/21_debug.c
+++ Library/demos/21_debug/C/21_debug.c
@@ -1,3 +1,4 @@
+#include "../includes/21_debug.h"
 #include "graphics/video.h"
 
 int main() {
---
+++ Library/demos/21_debug/includes/21_debug.h
@@ -0,0 +1,6 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+// Add more project-specific macros or declarations here
+
+#endif // 21_DEBUG_H
