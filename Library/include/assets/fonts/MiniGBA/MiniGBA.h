9e8a9d17c70fd9e15b85ef6568424d89db336bb8
grAyDHD <graydhdsquared@gmail.com>
2025-05-28 14:59:55 -0400

    added font image file

---
+++ Library/include/assets/fonts/MiniGBA/MiniGBA.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(MiniGBA)
+
+//======================================================================
+//
+//	MiniGBA, 635x11@16, 
+//	+ bitmap not compressed
+//	Total size: 13992 = 13992
+//
+//	Time-stamp: 2025-05-28, 13:54:49
+//	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_MINIGBA_H
+#define GRIT_MINIGBA_H
+
+#define MiniGBABitmapLen 13992
+extern const unsigned short MiniGBABitmap[6996];
+
+#endif // GRIT_MINIGBA_H
+
+//}}BLOCK(MiniGBA)
