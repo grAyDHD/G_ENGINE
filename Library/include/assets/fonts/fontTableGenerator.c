a9844909dcb521bd6fb366f5ac2ea78b807f249c
grAyDHD <graydhdsquared@gmail.com>
2025-05-31 20:09:15 -0400

    outlined fontTableGenerator in comments of fontTableGenerator.c, will iterate in small steps until finished

---
+++ Library/include/assets/fonts/fontTableGenerator.c
@@ -0,0 +1,22 @@
+#define ROW_LENGTH 633
+#define COL_HEIGHT 11 // once generator works, use scanf for w/h
+
+  // Program creation steps:
+  // Load in data table
+
+int main() {
+  return 0;
+}
+
+  // Goal of program:
+  // create data table used for rendering glyphs from print functions
+  
+  // Program flow:
+  // run ./fontTableGenerator FontName
+  // program searches FontName directory for FontName.c
+  // imports array in FontName.c
+  // processes array into 2d array of 633 columns and 11 rows
+  // creates fontData file with unpopulated array, or initialized to zero, whichever makes more sense/is easier
+  // then parses through column by column to begin process of populating fontData array
+  // saves changes to file once array is fully populated
+
