a3b37fe85ad742411aafd8ca2be6972b95c2f574
grAyDHD <graydhdsquared@gmail.com>
2025-06-02 15:40:19 -0400

    added PAUSE_MENU game state.

--- Library/demos/20_m3UIFramework/C/20_m3UIFramework.c
+++ Library/demos/20_m3UIFramework/C/20_m3UIFramework.c
@@ -1,14 +1,27 @@
 #include "../includes/ui.h"
 #include "../build/Bedroom.h"
 #include "../build/Robo.h"
+#include "input/in.h"
 #include "ecs/entities.h"
 #include "graphics/draw.h"
+#include "input/in.h"
 
 static ComponentStorage components;
 static ECS ecs;
 
 volatile fixed_s32 deltaTime;
 
+typedef enum {
+  PLAYING,
+  PAUSE_MENU
+} GameState;
+
+typedef enum {
+  VOLUME,
+  CHARACTER_SELECTION,
+  // etc.
+
+} MenuState;
 
 int main() {
   DSPC = MODE3 | BG2;
@@ -19,24 +32,40 @@ int main() {
   ecs.entity[0].flag |= PHYSICS_FLAG; //ON_GROUND;
   createScreenBorders(&ecs);
 
+  GameState state = PLAYING;
+
   while (1) {
     VBLANK();
-
     fillScreen(0x0000);
 
-    updateInputSystem(&ecs, ecs.entity, ecs.components->input, deltaTime);
-    updateBehaviorSystem(&ecs, ecs.entity, ecs.components->ai);
-    updatePhysicsSystem(ecs.entity, ecs.components->velocity,
+
+    if(keyTapped(ST)) {
+//      ecs.entity[0].flag &= ~ENABLE_INPUT;
+      state = PAUSE_MENU;
+      // if game PLAYING, set state to PAUSE_MENU  
+      //   disable player input by removing INPUT_ENABLED flag
+      // else if game state PAUSE_MENU, set state to PLAYING
+      //   enable player input again 
+
+    }
+
+    //  Main PLAYING loop below
+    switch(state) {
+      case(PLAYING):
+      updateInputSystem(&ecs, ecs.entity, ecs.components->input, deltaTime);
+      updateBehaviorSystem(&ecs, ecs.entity, ecs.components->ai);
+      updatePhysicsSystem(ecs.entity, ecs.components->velocity,
                         ecs.components->acceleration, deltaTime);
-    updateMovementSystem(ecs.entity, ecs.components->position,
+      updateMovementSystem(ecs.entity, ecs.components->position,
                          ecs.components->velocity, deltaTime);
-    updateCollisionSystem(ecs.entity, ecs.components->position,
+      updateCollisionSystem(ecs.entity, ecs.components->position,
                           ecs.components->velocity, ecs.components->hitbox,
                           deltaTime);
-    updateAnimationSystem(ecs.entity, ecs.components->animation);
-    updateRenderSystem(&ecs, ecs.entity, ecs.components->animation,
+      updateAnimationSystem(ecs.entity, ecs.components->animation);
+      updateRenderSystem(&ecs, ecs.entity, ecs.components->animation,
                        ecs.components->draw);
-    deltaTime = 0;
+      deltaTime = 0;
+    }
   }
 
   return 0;
