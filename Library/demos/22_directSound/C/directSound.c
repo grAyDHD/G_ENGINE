9dbeb2c01f90759bd636b4f65e67a4f8b24d5d21
grAyDHD <graydhdsquared@gmail.com>
2025-06-29 21:41:11 -0400

    added in ISR for incrementing an overflow to track song length and stop dma playback

--- Library/demos/22_directSound/C/directSound.c
+++ Library/demos/22_directSound/C/directSound.c
@@ -8,30 +8,39 @@
 #include "../includes/FTGFull.h"
 
 const u32* audioSample = (u32 *)FTG_raw;  //the sample, 8bit signed, 16Khz
-
-void DmaPlaySound (void);
+void DmaPlaySound(void);
+void tmr1_isr(void);
  
 int main(void){
-//	play a sound using timer 0 as sampling source
-//	When timer 0 overflows, the interrupt handler loads the FIFO with the next sample
-
   DSPC = MODE3 | BG2;
 
 	while(1){
     updateKeys();
 		if(keyTapped(A | B)){
-			TIMER[0].control=0;	//disable timer 0
+			TIMER[0].control = 0;	//disable timer 0
 			DmaPlaySound(); //play in dma mode
 		}
 	};
 }
 
+int overflow = 0;
+void tmr1_isr() {
+  if (overflow == 43) {
+    DMA[1].control = 0;
+    TIMER[0].control = 0;
+    TIMER[1].control = 0;
+    overflow = 0;
+  } else {
+    overflow++;
+  }
+  irqAcknowledge(IRQ_TMR1);
+}
 
 void DmaPlaySound (void){
 	//Play a mono sound at 16khz in DMA mode Direct Sound
 	//uses timer 0 as sampling rate source
 	//uses timer 1 to count the samples played in order to stop the sound 
-//  ISR = InterruptProcess;
+  ISR = tmr1_isr;
 
   AUDIO->ds = DS_INIT | DSA_FIFO_RESET | DSB_FIFO_RESET;
   AUDIO->master = AUDIO_MASTER_ENABLE;
@@ -40,14 +49,15 @@ void DmaPlaySound (void){
   DMA[1].destination = (u32)FIFO_A;
   DMA[1].control = DMA_ENABLE | DMA_32BIT | DMA_START_SPECIAL | DMA_REPEAT;
 	
-//  TIMER[1].value = 0x7098; // 0xffff - number of samples to play, use inline function with parameter of sample number
-//  TIMER[1].control = TMR_ENABLE | /* TMR_IRQ_ENABLE |*/ TMR_CASCADE;
+  //for shorter audio, 0xFFFF - number of samples
+  TIMER[1].value = 0;
+  TIMER[1].control = TMR_ENABLE | TMR_IRQ_ENABLE | TMR_CASCADE;
 
- // irqEnable(IRQ_TMR1);
- // irqMaster(ON);
+  irqEnable(IRQ_TMR1);
+  irqMaster(ON);
 	
-	//Formula for playback frequency is: 0xFFFF-round(cpuFreq/playbackFreq)
+	//Formula for playback frequency: 0xFFFF - (cpuFreq/playbackFreq)
+  // cpuFreq = 2^24
   TIMER[0].value = 0xFBE8; // 16khz playback freq
-  TIMER[0].control = TMR_ENABLE; //enable at cpu freq?  What does this mean
-				 
+  TIMER[0].control = TMR_ENABLE;				 
 }
