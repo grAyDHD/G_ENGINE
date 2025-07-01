d5f1f2e9febb709dc971410590124d136d043850
grAyDHD <graydhdsquared@gmail.com>
2025-07-01 16:29:33 -0400

    audio mixer design doc created

--- Library/demos/22_directSound/C/directSound.c
+++ Library/demos/22_directSound/C/directSound.c
@@ -5,11 +5,13 @@
 #include "core/dma.h"
 #include "core/interrupts.h"
 #include "audio/audio.h"
-#include "../includes/FTG_L.h"
-#include "../includes/FTG_R.h"
+//#include "../includes/FTG_L.h"
+//#include "../includes/FTG_R.h"
 
-// todo: create interface for song selection, pre process songs to play well with gba audio make less muddy
 // todo: move audio data to c files and out of headers
+s8 mixBufferA[64];
+s8 mixBufferB[64];
+
 const u32* audioLeft = (u32 *)FTG_L_raw;  //the sample, 8bit signed, 16Khz
 const u32* audioRight = (u32 *)FTG_R_raw;  //the sample, 8bit signed, 16Khz
 void DmaPlaySound(void);
@@ -20,12 +22,15 @@ int main(void){
 
 	while(1){
     updateKeys();
-		if(keyTapped(A | B)){
-			DmaPlaySound(); //play in dma mode
+    // create bgPlaying var?  if playing, stop, if not, start.
+		if(keyTapped(ST)){
+			DmaPlaySound(); //rename function, will play a full song
 		}
 	}
 }
 
+
+/*
 int overflow = 0;
 void tmr1_isr() {
   if (overflow == 43) {
@@ -38,15 +43,14 @@ void tmr1_isr() {
   }
   irqAcknowledge(IRQ_TMR1);
 }
+*/
 
 void DmaPlaySound (void){
-	//Play a mono sound at 16khz in DMA mode Direct Sound
-	//uses timer 0 as sampling rate source
-	//uses timer 1 to count the samples played in order to stop the sound 
-  ISR = tmr1_isr;
+//  ISR = tmr1_isr;
 
   AUDIO->ds = DS_STEREO_INIT | DSA_FIFO_RESET | DSB_FIFO_RESET | DSA_TMR(0) | DSB_TMR(0);
   AUDIO->master = AUDIO_MASTER_ENABLE;
+  
 	
   DMA[1].source = (u32)audioLeft;
   DMA[1].destination = (u32)FIFO_A;
@@ -56,13 +60,15 @@ void DmaPlaySound (void){
   DMA[2].destination = (u32)FIFO_B;
   DMA[2].control = DMA_ENABLE | DMA_32BIT | DMA_START_SPECIAL | DMA_REPEAT;
 
-  TIMER[1].value = 0; //for shorter audio, 0xFFFF - number of samples and stop on irq
-  TIMER[1].control = TMR_ENABLE | TMR_IRQ_ENABLE | TMR_CASCADE;
+
+	//Formula for playback frequency: 0xFFFF - (cpuFreq/playbackFreq)
+  TIMER[0].value = 0xFBE8; // 16khz playback freq
+  TIMER[0].control = TMR_ENABLE;				 
 
   irqEnable(IRQ_TMR1);
   irqMaster(ON);
 	
-	//Formula for playback frequency: 0xFFFF - (cpuFreq/playbackFreq)
-  TIMER[0].value = 0xFBE8; // 16khz playback freq
-  TIMER[0].control = TMR_ENABLE;				 
+ // TIMER[1].value = 0; //for shorter audio, 0xFFFF - number of samples and stop on irq
+  TIMER[1].value = 0xFFFF - 128;
+  TIMER[1].control = TMR_ENABLE | TMR_IRQ_ENABLE | TMR_CASCADE;
 }
