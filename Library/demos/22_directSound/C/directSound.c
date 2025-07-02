8e5b35e197c29316988271948e2ac914c22d8f34
grAyDHD <graydhdsquared@gmail.com>
2025-07-02 02:55:58 -0400

    cool weird audio bug will fix later

--- Library/demos/22_directSound/C/directSound.c
+++ Library/demos/22_directSound/C/directSound.c
@@ -1,24 +1,40 @@
 #include "../includes/directSound.h"
+#include "core/dma.h"
 
-const u8 singleBuffer[256] = {0};
+u8 singleBuffer[256] = {0};
+u32 tharpPosition = 0;
 
 void isr(void) {
-  DMA[1].source = (u32)singleBuffer; // reads 4 samples at once from DMA, so must increment position in buffer.
+  // Fill buffer with next chunk of audio
+  if (tharpPosition < tharp16klen - 256) {
+    Dma3(singleBuffer, tharp16k + tharpPosition, 64, DMA_MEMCPY32);
+    tharpPosition += 256;
+  } else {
+    // End of sample - could loop or stop
+    tharpPosition = 0;  // Loop back to beginning
+    Dma3(singleBuffer, tharp16k, 64, DMA_MEMCPY32);
+    tharpPosition += 256;
+  }
+  
+  // Restart DMA to play the newly filled buffer
   DMA[1].source = (u32)singleBuffer;
-  DMA[1].wordCount = 64; 
-  DMA[1].destination = (u32)FIFO_A;
   DMA[1].control = DMA_ENABLE | DMA_32BIT | DMA_START_SPECIAL | DMA_IRQ_ENABLE;
   irqAcknowledge(IRQ_DMA1);
 }
 
 int main(void){
+  DSPC = MODE3 | BG2;
 
+  // prefill buffer with audio
+  Dma3(singleBuffer, tharp16k + tharpPosition, 64, DMA_MEMCPY32);
+  tharpPosition += 256;
+
+  // setup interrupts/dma1
   ISR = isr;
   irqEnable(IRQ_DMA1);
   irqMaster(ON);	
   
-  DSPC = MODE3 | BG2;
-
+  // Initiate audio playback
   AUDIO->ds = DS_MONO_INIT | DSA_TMR(0);
   AUDIO->master = AUDIO_MASTER_ENABLE;
   DMA[1].source = (u32)singleBuffer;
