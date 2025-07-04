b0a2dbf2d384beec0bbb2d993cbaef9b1bdf819f
grAyDHD <graydhdsquared@gmail.com>
2025-07-03 22:10:56 -0400

    Revert "aReapply "implemented single buffered audio on timer, double buffer next""
    This reverts commit 24f3db5941ed444cfe7e4e2bc2fb2b9769f2139b.

--- Library/demos/22_directSound/C/directSound.c
+++ Library/demos/22_directSound/C/directSound.c
@@ -1,14 +1,6 @@
 #include "../includes/directSound.h"
-#include "audio/audio.h"
-#include "audio/dmg.h"
 #include "core/dma.h"
-#include "core/interrupts.h"
-#include "core/timer.h"
 
-	//Formula for playback frequency: 0xFFFF - (cpuFreq/playbackFreq)
-
-#define BUFFER_SIZE 256
-#define ENABLE_AUDIO (AUDIO->master = AUDIO_MASTER_ENABLE)
 
 typedef struct {
   const s8 *data;
@@ -17,55 +9,47 @@ typedef struct {
 } AudioChannel;
 
 AudioChannel channel[4];
-s8 singleBuffer[BUFFER_SIZE] = {0};
 
-void initMixChannels() {
+void initAudioChannel() {
   channel[0].data = tharp16k;   
   channel[0].position = 0;
   channel[0].length = tharp16klen;
 }
 
-
-void tm_isr(void) {
-  if (channel[0].position < channel[0].length - BUFFER_SIZE) {
-    Dma3(singleBuffer, channel[0].data + channel[0].position, BUFFER_SIZE/4, DMA_MEMCPY32);
-    channel[0].position += BUFFER_SIZE;
+s8 singleBuffer[256] = {0};
+void isr(void) {
+  // Fill buffer with next chunk of audio
+  if (channel[0].position < channel[0].length - 16) {
+    //fill buffer with audio from audio's current position
+    Dma3(singleBuffer, channel[0].data + channel[0].position, 4, DMA_MEMCPY32);
+    channel[0].position += 16;
   } else {
-    // End of sample - loop back to beginning
-    channel[0].position = 0;
-    Dma3(singleBuffer, channel[0].data, BUFFER_SIZE/4, DMA_MEMCPY32);
+    // End of sample - could loop or stop
+    channel[0].position = 0;  // Loop back to beginning
+    Dma3(singleBuffer, channel[0].data, 4, DMA_MEMCPY32);
   }
-
-  DMA[1].control = 0;  // Disable completely  
-  DMA[1].control = DMA_ENABLE | DMA_START_SPECIAL | DMA_REPEAT | DMA_DEST_FIXED;  // Full restart
-    
-  irqAcknowledge(IRQ_TMR1);
+  
+//  DMA[1].control = DMA_ENABLE | DMA_START_SPECIAL | DMA_IRQ_ENABLE;
+  irqAcknowledge(IRQ_DMA1);
 }
 
-//only sets up direct sound A right now, begins reading from buffer
-void initMonoFIFO() {
-  ENABLE_AUDIO;
-  AUDIO->ds = DS_MONO_INIT | DSA_TMR(0);
-
-  DMA[1].source = (u32)singleBuffer;
-  DMA[1].destination = (u32)FIFO_A;
-  DMA[1].control = DMA_ENABLE | DMA_START_SPECIAL; 
-
-  TIMER[0].value = 0xFBE8;
-  TIMER[0].control = TMR_ENABLE;				 
-
-  TIMER[1].value = (0xFFFF - 256);
-  TIMER[1].control = TMR_CASCADE | TMR_ENABLE | TMR_IRQ_ENABLE;
-
-  ISR = tm_isr;
-  irqEnable(IRQ_TMR1);
-}
 int main(void){
   DSPC = MODE3 | BG2;
-  initMixChannels();
-  initMonoFIFO(); // is now reading from buffer
+  initAudioChannel();
+  ISR = isr;
+  irqEnable(IRQ_DMA1);
   irqMaster(ON);	
+
+  AUDIO->ds = DS_MONO_INIT | DSA_TMR(0);
+  AUDIO->master = AUDIO_MASTER_ENABLE;
+  DMA[1].source = (u32)singleBuffer;
+  DMA[1].destination = (u32)FIFO_A;
+  DMA[1].control = DMA_ENABLE | DMA_START_SPECIAL | DMA_IRQ_ENABLE | DMA_REPEAT;
   
+	//Formula for playback frequency: 0xFFFF - (cpuFreq/playbackFreq)
+  TIMER[0].value = 0xFBE8; // 16khz playback freq, controls DMA repeat rate
+  TIMER[0].control = TMR_ENABLE;				 
+
 	while(1){}
   return 0;
 }
