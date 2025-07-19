bcf5c04703112545a64d9d1f010372442dfa27e8
Grayson <graydhdsquared@gmail.com>
2025-07-19 17:37:12 -0500

    initialized mod player main.c

--- Library/demos/23_modPlayer/C/23_modPlayer.c
+++ Library/demos/23_modPlayer/C/23_modPlayer.c
@@ -1,8 +1,21 @@
 #include "../includes/23_modPlayer.h"
+#include "audio/mixer.h"
+#include "core/interrupts.h"
 #include "graphics/video.h"
 
 int main() {
-    DSPC = MODE3 | BG2;
+  DSPC = MODE3 | BG2;
 
-    return 0;
+  initMixChannels();
+
+  irqMaster(ON);  // now enable interrupts
+  initMonoFIFO(); // is now reading from buffer
+
+  while (1) {
+    if (reload == 1) {
+      mixAudio();
+      reload = 0;
+    }
+  }
+  return 0;
 }
