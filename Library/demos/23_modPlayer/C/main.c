dddb1215830587cc5730414056b17732afcde7ab
Grayson <graydhdsquared@gmail.com>
2025-07-31 16:13:09 -0500

    stripped down mod player demo slightly

--- Library/demos/23_modPlayer/C/main.c
+++ Library/demos/23_modPlayer/C/main.c
@@ -5,6 +5,8 @@
 #include "graphics/m3Text.h"
 #include "graphics/video.h"
 
+#define DSPS (*(volatile u16 *)0x04000004)
+
 int main() {
   DSPC = MODE3 | BG2;
 
@@ -62,60 +64,69 @@ int main() {
     if (keyTapped(B)) {
       playMod(MOD_BIT_Introtune);
     }
+    /*
+        // 1. Channel 0 data pointer
+        if (modMixerChannel[0].data != (s8 *)lastData) {
+          clearTextArea(127, 35, 55, 9);
+          gprintf(105, 35, "%x", (u32)modMixerChannel[0].data);
+          lastData = (u32)modMixerChannel[0].data;
+        }
+        // UPDATE WARNING
+        // 2. Channel 0 position
+        if (modMixerChannel[0].pos != lastPos && DSPS & 0x0001) {
+          clearTextArea(127, 45, 55, 9);
+          gprintf(105, 45, "%x", modMixerChannel[0].pos);
+          lastPos = modMixerChannel[0].pos;
+        }
 
-    // 1. Channel 0 data pointer
-    if (modMixerChannel[0].data != (s8 *)lastData) {
-      clearTextArea(127, 35, 55, 9);
-      gprintf(105, 35, "%x", (u32)modMixerChannel[0].data);
-      lastData = (u32)modMixerChannel[0].data;
-    }
-    // UPDATE WARNING
-    // 2. Channel 0 position
-    if (modMixerChannel[0].pos != lastPos && frameCount % 5 == 0) {
-      clearTextArea(127, 45, 55, 9);
-      gprintf(105, 45, "%x", modMixerChannel[0].pos);
-      lastPos = modMixerChannel[0].pos;
-    }
-    // 3. Channel 0 increment
-    if (modMixerChannel[0].inc != lastInc) {
-      clearTextArea(127, 55, 55, 9);
-      gprintf(105, 55, "%x", modMixerChannel[0].inc);
-      lastInc = modMixerChannel[0].inc;
-    }
-    // 4. Channel 0 volume
-    if (modMixerChannel[0].vol != lastVol && frameCount % 5 == 1) {
-      clearTextArea(127, 65, 55, 9);
-      gprintf(105, 65, "%x", modMixerChannel[0].vol);
-      lastVol = modMixerChannel[0].vol;
-    }
-    // 5. Channel 0 length
-    if (modMixerChannel[0].length != lastLength) {
-      clearTextArea(127, 75, 55, 9);
-      gprintf(105, 75, "%x", modMixerChannel[0].length);
-      lastLength = modMixerChannel[0].length;
-    }
-    // 6. Channel 0 loop length
-    if (modMixerChannel[0].loopLength != lastLoopLength) {
-      clearTextArea(127, 85, 55, 9);
-      gprintf(105, 85, "%x", modMixerChannel[0].loopLength);
-      lastLoopLength = modMixerChannel[0].loopLength;
-    }
-    // 7. Current order
-    if (modPlayer.curOrder != lastCurOrder) {
-      clearTextArea(127, 105, 55, 9);
-      gprintf(105, 105, "%x", modPlayer.curOrder);
-      lastCurOrder = modPlayer.curOrder;
-    }
-    // UPDATE WARNING
-    // 8. Current row
-    if (modPlayer.curRow != lastCurRow && frameCount % 5 == 2) {
-      clearTextArea(127, 115, 55, 9);
-      gprintf(105, 115, "%x", modPlayer.curRow);
-      lastCurRow = modPlayer.curRow;
-    }
+        // 3. Channel 0 increment
+        if (modMixerChannel[0].inc != lastInc) {
+          clearTextArea(127, 55, 55, 9);
+          gprintf(105, 55, "%x", modMixerChannel[0].inc);
+          lastInc = modMixerChannel[0].inc;
+        }
+
+        // 4. Channel 0 volume
+        if (modMixerChannel[0].vol != lastVol && frameCount % 5 == 1 &&
+            DSPS & 0x0001) {
+          clearTextArea(127, 65, 55, 9);
+          gprintf(105, 65, "%x", modMixerChannel[0].vol);
+          lastVol = modMixerChannel[0].vol;
+        }
+
+        // 5. Channel 0 length
+        if (modMixerChannel[0].length != lastLength) {
+          clearTextArea(127, 75, 55, 9);
+          gprintf(105, 75, "%x", modMixerChannel[0].length);
+          lastLength = modMixerChannel[0].length;
+        }
+
+        // 6. Channel 0 loop length
+        if (modMixerChannel[0].loopLength != lastLoopLength) {
+          clearTextArea(127, 85, 55, 9);
+          gprintf(105, 85, "%x", modMixerChannel[0].loopLength);
+          lastLoopLength = modMixerChannel[0].loopLength;
+        }
+
+        // 7. Current order
+        if (modPlayer.curOrder != lastCurOrder) {
+          clearTextArea(127, 105, 55, 9);
+          gprintf(105, 105, "%x", modPlayer.curOrder);
+          lastCurOrder = modPlayer.curOrder;
+        }
+
+        // UPDATE WARNING
+        // 8. Current row
+        if (modPlayer.curRow != lastCurRow && frameCount % 5 == 2 &&
+            DSPS & 0x0001) {
+          clearTextArea(127, 115, 55, 9);
+          gprintf(105, 115, "%x", modPlayer.curRow);
+          lastCurRow = modPlayer.curRow;
+        }
+
     // UPDATE WARNING
     // 9. Current tick
-    if (modPlayer.tick != lastTick && frameCount % 5 == 3) {
+    if (modPlayer.tick != lastTick) {
       clearTextArea(127, 125, 55, 9);
       gprintf(105, 125, "%x", modPlayer.tick);
       lastTick = modPlayer.tick;
@@ -126,44 +137,19 @@ int main() {
       gprintf(105, 135, "%x", modPlayer.state);
       lastState = modPlayer.state;
     }
+      */
+
+    /*
     // UPDATE WARNING
     // 11. Samples until tick
-    if (modTiming.samplesUntilMODTick != lastSamplesToTick &&
-        frameCount % 5 == 4) {
+    if (modTiming.samplesUntilMODTick != lastSamplesToTick) {
       clearTextArea(127, 145, 55, 9);
       gprintf(105, 145, "%x", modTiming.samplesUntilMODTick);
       lastSamplesToTick = modTiming.samplesUntilMODTick;
     }
-
+*/
     frameCount++;
   }
 
   return 0;
 }
-
-/*
-    // Print out some info on the screen
-    FontPrint(0, 4, BGS(31), "C0.data:");
-    FontPrint(8, 4, BGS(31), htoa((s32)sndChannel[0].data));
-    FontPrint(0, 5, BGS(31), "C0.pos:");
-    FontPrint(8, 5, BGS(31), htoa((s32)sndChannel[0].pos));
-    FontPrint(0, 6, BGS(31), "C0.inc:");
-    FontPrint(8, 6, BGS(31), htoa((s32)sndChannel[0].inc));
-    FontPrint(0, 7, BGS(31), "C0.vol:");
-    FontPrint(8, 7, BGS(31), htoa((s32)sndChannel[0].vol));
-    FontPrint(0, 8, BGS(31), "C0.len:");
-    FontPrint(8, 8, BGS(31), htoa((s32)sndChannel[0].length));
-    FontPrint(0, 9, BGS(31), "C0.loop:");
-    FontPrint(8, 9, BGS(31), htoa((s32)sndChannel[0].loopLength));
-
-    FontPrint(0, 11, BGS(31), "mod.curOrder:");
-    FontPrint(15, 11, BGS(31), htoa(sndMod.curOrder));
-    FontPrint(0, 12, BGS(31), "mod.curRow:");
-    FontPrint(15, 12, BGS(31), htoa(sndMod.curRow));
-    FontPrint(0, 13, BGS(31), "mod.tick:");
-    FontPrint(15, 13, BGS(31), htoa(sndMod.tick));
-    FontPrint(0, 14, BGS(31), "mod.state:");
-    FontPrint(15, 14, BGS(31), htoa(sndMod.state));
-    FontPrint(0, 15, BGS(31), "smpsToTick:");
-    FontPrint(15, 15, BGS(31), htoa(sndVars.samplesUntilMODTick));
-*/
